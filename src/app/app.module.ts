beb32c9cbe872096a9f7092dc5285eb6889df624
Himanshu Kumar <himanshu28051997@gmail.com>
2017-12-26 15:14:07 +0530

    Reactive forms part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { LeaderService } from './services/leader.service';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms'; 
 @NgModule({
   declarations: [
     AppComponent,
@@ -43,7 +44,8 @@ import { FormsModule } from '@angular/forms';
     MaterialModule,
     FlexLayoutModule,
 	AppRoutingModule,
-	FormsModule
+	FormsModule,
+	ReactiveFormsModule
   ],
   entryComponents: [
         LoginComponent
