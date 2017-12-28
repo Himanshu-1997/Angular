5829531e519b3d664f02b7ca43cc5466b6e063c2
Himanshu Kumar <himanshu28051997@gmail.com>
2017-12-28 23:30:21 +0530

    Reactive Forms part 3 completed

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 import { LEADERS } from '../shared/leaders';
 import { Leader } from '../shared/leader';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+
+import 'rxjs/add/operator/delay';
 
 @Injectable()
 export class LeaderService {
-	getFeaturedLeader(): Promise<Leader> {
-    return new Promise (resolve=> { setTimeout(() => resolve(LEADERS.filter((leader) => leader.featured)[0]),2000);});
+	getFeaturedLeader(): Observable<Leader> {
+    return Observable.of(LEADERS.filter((leader) => leader.featured)[0]).delay(2000);
   }
   constructor() { }
 
